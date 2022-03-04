8bc39ac0a67d932d7ea91cfe98e1364e76621beb
B1906812 <italia05100510@gmail.com>
2022-03-04 16:00:06 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const { BadRequestError, errorHandler } = require("./app/errors");
+
 const app = express();
+
 const setupContactRoutes = require("./app/routes/contact.routes");
+
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -12,5 +18,17 @@ app.get("/",(req, res) => {
 });
 
 setupContactRoutes(app);
+
+
+//handle 404 response
+app.use((req, res, next) => {
+        
+        next(new BadRequestError(404, "Resource not found"));
+});
+
+//define error-handling middleware last, after other app.use() and routes calls 
+app.use((err, req, res, next) => {
+        errorHandler.handleError(err, res);
+});
 
 module.exports = app; 
